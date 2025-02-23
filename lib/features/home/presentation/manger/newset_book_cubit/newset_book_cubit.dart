7c3f0d62676476ed0a48e621c5ee094788484896
zakaryajawish <zakaryajawish@gmail>
2025-02-23 09:29:54 +0300

    Implement fetchNewsetBooks method in NewsetBookCubit

--- lib/features/home/presentation/manger/newset_book_cubit/newset_book_cubit.dart
+++ lib/features/home/presentation/manger/newset_book_cubit/newset_book_cubit.dart
@@ -2,9 +2,21 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 import '../../../data/models/book_model/book_model.dart';
+import '../../../data/repos/home_repo.dart';
 
 part 'newset_book_state.dart';
 
 class NewsetBookCubit extends Cubit<NewsetBookState> {
-  NewsetBookCubit() : super(NewsetBookInitial());
+  NewsetBookCubit(this.homeRepo) : super(NewsetBookInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBookLoadingState());
+    var result = await homeRepo.fectchNewsetBooks();
+    result.fold((failure) {
+      emit(NewsetBookFailureState(failure.errMessage));
+    }, (books) {
+      emit(NewsetBookSuccessState(books: books));
+    });
+  }
 }
