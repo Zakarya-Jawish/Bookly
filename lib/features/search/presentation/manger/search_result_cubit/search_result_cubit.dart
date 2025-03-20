122a945b92a6b9c24c85391ac3b00d128953d1fd
zakaryajawish <zakaryajawish@gmail>
2025-03-20 16:11:04 +0300

    implement search cubit

--- lib/features/search/presentation/manger/search_result_cubit/search_result_cubit.dart
+++ lib/features/search/presentation/manger/search_result_cubit/search_result_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/core/models/book_model/book_model.dart';
+import 'package:bookly/features/search/data/repos/search_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'search_result_state.dart';
 
 class SearchResultCubit extends Cubit<SearchResultState> {
-  SearchResultCubit() : super(SearchResultInitial());
+  SearchResultCubit(this.searchRepo) : super(SearchResultInitial());
+  final SearchRepo searchRepo;
+  Future<void> fetechSearchResult({required String subject}) async {
+    emit(SearchResultLoadingState());
+    var result = await searchRepo.fetchSearchResult(subject: subject);
+    result.fold((failure) {
+      emit(SearchResultFailureState(failure.errMessage));
+    }, (books) {
+      emit(SearchResultSuccessState(books: books));
+    });
+  }
 }
